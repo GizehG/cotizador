607f50f046f8ed3348d17e6aaf19e22369e10d38
GizehG <gizeh.garcia@galileo.edu>
2022-05-06 13:58:28 -0600

    Adding SQL Script and E/R Squema

---
+++ cotizadorBK/src/test/java/com/example/cotizadorBK/CotizadorBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cotizadorBK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotizadorBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
